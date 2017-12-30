9aac52f5bdf7bd95fa4c231c98d1b729b6a2e7b3
Paulo Vale <paulogaspar404@gmail.com.br>
2017-12-30 17:46:40 -0200

    Fixed some bugs i left at the input and fader classes. Now they are working as i wished. Added a screen class to use as a parent for all the stages to manage some input class stuff. Started using TODOS.

---
+++ core/src/com/pgsv/game/stages/Screen.java
@@ -0,0 +1,45 @@
+package com.pgsv.game.stages;
+
+import com.pgsv.game.utils.Input;
+
+public class Screen implements com.badlogic.gdx.Screen {
+
+    public void update(float delta){
+        Input.listen();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
